96d73d743b8527d8c78bfa13d8f3e12972707f2d
Marlon McPherson <mmcpherson5@cnm.edu>
2018-05-08 13:56:30 -0600

    added project files. Added ipsum to index.html

---
+++ script.js
@@ -0,0 +1,8 @@
+var shizz = document.getElementById('btn');
+
+shizz.addEventListener('click', function() {
+	var sty = document.getElementById('btn');
+	sty.style.color = "red";
+	console.log("sfsgs");
+
+});
