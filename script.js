bfe56d77091fa320f4468c448a3ca188c8a85678
Marlon McPherson <mmcpherson5@cnm.edu>
2018-05-08 20:51:46 -0600

    linked JS to HTML

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-var shizz = document.getElementById('btn');
 
-shizz.addEventListener('click', function() {
-	var sty = document.getElementById('btn');
-	sty.style.color = "red";
-	console.log("sfsgs");
+let para = document.getElementById("one");
+para.addEventListener("mouse-over", function(){ paraOne.style.color = "red";});
 
-});
+console.log("connected");
