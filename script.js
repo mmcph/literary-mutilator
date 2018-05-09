3cf9a55922f5c9f3988b895b286557b7669b24fb
Marlon McPherson <mmcpherson5@cnm.edu>
2018-05-08 21:43:35 -0600

    completed JS event listeners.

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
+let div = document.getElementById("container");
+let paraOne = document.getElementById("one");
+let paraTwo = document.getElementById("two");
+let paraThree = document.getElementById("three");
+let paraThreeSave = document.getElementById("three").innerText;
+let paraFour = document.getElementById("four");
 
-let para = document.getElementById("one");
-para.addEventListener("mouse-over", function(){ paraOne.style.color = "red";});
 
-console.log("connected");
+paraOne.addEventListener("click", function(){
+
+	paraOne.style.color = "red";
+
+});
+
+paraOne.addEventListener("dblclick", function(){
+
+	paraOne.style.fontSize = "20px";
+
+});
+
+paraTwo.addEventListener("mouseenter", function(){
+
+	let newTxt = paraTwo.innerText;
+	paraThree.innerText = newTxt;
+
+});
+
+paraTwo.addEventListener("mouseleave", function(){
+
+	paraThree.innerText = paraThreeSave;
+
+});
+
+
+div.addEventListener("click", function() {
+
+	let orig = paraFour.innerText;
+	let newTxt = orig.replace("cliche", "hipster");
+	paraFour.innerText = newTxt;
+
+});
+
